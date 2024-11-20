609be9f6bdbbe2ef34821f80719d4fce87bd2f29
Archit Garg <architgarg2003@hotmail.com>
2024-11-21 03:49:50 +0530

    gunicorn

--- app.py
+++ app.py
@@ -690,4 +690,5 @@ def health_check():
     }), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    # app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
