4e2789af43870d42fe96165507ab9649e091bcd1
Archit Garg <architgarg2003@hotmail.com>
2025-03-23 15:15:15 +0530

    a

--- app.py
+++ app.py
@@ -242,7 +242,7 @@ def health_check():
     }), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0', port=5000)
     # app.run(host='0.0.0.0', port=int(os.getenv('PORT', 5001)))
 
 
